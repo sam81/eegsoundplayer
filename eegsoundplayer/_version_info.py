712283cbf579079b9d367a07c26dc74b90af6fca
Samuele Carcagno <sam.carcagno@gmail.com>
2015-09-26 23:07:54 +0100

    fix deb

--- eegsoundplayer/_version_info.py
+++ eegsoundplayer/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-eegsoundplayer_version = "0.0.5"
-eegsoundplayer_builddate = "26-Sep-2015 22:56"
+eegsoundplayer_version = "0.0.6"
+eegsoundplayer_builddate = "26-Sep-2015 23:07"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import glob
 setup(
     name="eegsoundplayer",
-    version="0.0.5",
+    version="0.0.6",
     url="",
     author="Samuele Carcagno",
     author_email="sam.carcagno@google.com;",
